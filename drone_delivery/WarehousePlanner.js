b73d886d8e219d26cd7676505e82415582f80576
Gecataaaa <159815293+Gecataaaa@users.noreply.github.com>
2024-04-22 18:43:36 +0300

    Update WarehousePlanner.js

--- drone_delivery/WarehousePlanner.js
+++ drone_delivery/WarehousePlanner.js
@@ -2,10 +2,7 @@ class WarehousePlanner {
     constructor() {}
 
     findOptimalWarehouseLocations(customers, numWarehouses) {
-        // Implement algorithm to find optimal warehouse locations based on customer distribution
-        // This could involve clustering algorithms or other optimization techniques
-
-        // For simplicity, let's implement a basic algorithm that finds the centroid of all customer locations
+       
         let totalX = 0;
         let totalY = 0;
 
@@ -22,7 +19,7 @@ class WarehousePlanner {
         // Create 'numWarehouses' warehouses around the centroid
         const optimalLocations = [];
         for (let i = 0; i < numWarehouses; i++) {
-            // Add some randomness to the location for better distribution
+            
             const randomX = centroidX + Math.random() * 10 - 5; // Random number between -5 and 5
             const randomY = centroidY + Math.random() * 10 - 5; // Random number between -5 and 5
             optimalLocations.push({ x: randomX, y: randomY });
